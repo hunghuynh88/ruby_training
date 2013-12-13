7e3dbb692456f833a755b84265f62f45b053a01c
hung <hunghuynh201188@gmail.com>
2013-12-13 14:31:26 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyTraining::Application.config.secret_key_base = '9757caed28662e1a19332fb5cc9c58b885b7ce7ad0f48650a8523c1d969f444d62fdb386557034ef7d88894bdf817f45a417d4eab48ce25cf1a4ca3aebda0aa1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyTraining::Application.config.session_store :cookie_store, key: '_ruby_training_session'
